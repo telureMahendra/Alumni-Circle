6e4dd31ec5c9eff8e9f5f72154e8a0b032dd32e1
Mahendra Telure <teluremahendra@gmail.com>
2024-10-17 23:25:42 +0530

    Backend Code Updated

---
+++ AlumniCircle-server/src/main/java/com/geca/alumniCircle/AlumniCircleServerApplication.java
@@ -0,0 +1,13 @@
+package com.geca.alumniCircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniCircleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniCircleServerApplication.class, args);
+	}
+
+}
---
+++ AlumniCircle-server/src/test/java/com/geca/alumniCircle/AlumniCircleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geca.alumniCircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniCircleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
